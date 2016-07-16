96cd40cda911f7b12ae8a760f6f762163975b7be
Javed07 <javed.karim.dl@gmail.com>
2016-07-17 00:52:26 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Program for creating inverse of a matrix and caching it
+## makeCacheMatrix function reads a matrix and creates it's inverse
 
-## Write a short comment describing this function
+## setmatrix takes the value of matrix
+## getmatrix returns the matrix
+## setmatrixinverse takes the matrix and inverse it
+## getmatrixinverse returns the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   z <- solve(x)
@@ -23,7 +26,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function checks whether inverse of the matrix has been calculated or not. 
+## If cacheSolve function finds the inverse, it returns it from the caches.
+## If it does not find the invers the it calculates and returns it back.
 
 cacheSolve <- function(x, ...) {
   m <- x$getmatrixinverse()
@@ -34,5 +39,5 @@ cacheSolve <- function(x, ...) {
   read <- x$getmatrix()
   m <- matrix(read)
   x$setmatrixinverse(m)
-  x$getmatrixinverse()        ## Return a matrix that is the inverse of 'x'
+  x$getmatrixinverse()        
 }
